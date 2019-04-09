9680d4fea0895abc4609ae9cc4ce6ee683f11151
Ishan Sourav <44879608+ezioishan@users.noreply.github.com>
2019-04-09 14:31:16 +0530

    Basic webpage
    npm install express --save  --------------------> to add node modules to the Local Machine
    node app.js or node app    --------------------> to start the server
    **Above commands should be typed in cmd/bash from the directory where the uploaded files are located**

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.get('/',function(req, res){
+    res.send('You are at the index page!');
+});
+app.listen(port, () =>{
+    console.log(`Server started on port ${port}`);
+});
